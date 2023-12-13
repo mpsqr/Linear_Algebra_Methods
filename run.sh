b52a92eb4fa0dc1b60bcb12984384a8a6d238cdd
Matéo Pasquier <mateo.pasquier@uvsq.fr>
2023-12-13 15:14:33 +0100

    Added set_dense_RHS.

--- run.sh
+++ run.sh
@@ -7,12 +7,15 @@ cd tp/TP_Poisson_C_students_2022
 
 cp ambre.mk ./$(hostname).mk
 
-
 make all
 
 # Check if the environment works
 ./bin/tp_testenv
 
+
+# Run the code
+
+./bin/tpPoisson1D_direct
 
 # Clean
 rm $(hostname).mk
